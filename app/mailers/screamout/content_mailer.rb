6950b728d0ca19e6d0452839c38ff604104ae996
Shweta <shweta@joshsoftware.com>
2014-11-24 11:45:16 +0530

    Link to intranet in mail

--- app/mailers/screamout/content_mailer.rb
+++ app/mailers/screamout/content_mailer.rb
@@ -4,6 +4,7 @@ module Screamout
 
     def new_content_added(content_id)
       @content = Screamout::Content.where(id: content_id).first
+      @title = @content.title.blank? ? @content.url : @content.title
       mail(to: "all@joshsoftware.com", subject: "#{@content.user.public_profile.name.titleize} shared new content on intranet")
     end
   end
